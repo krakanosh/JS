c7908da4f0b80bac01edc942230f01e68f0fce0d
Lisin Alexandr <berserker37rus@gmail.com>
2021-10-06 17:30:18 +0300

    first commit

---
+++ js/action.js
@@ -0,0 +1,44 @@
+// alert("Привет");
+// var phone = prompt('Введите номер телефона', 8);
+// alert(phone);
+// let is18 = confirm("Вам исполнилось 18?");
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert(isBoss); // true, если нажата OK
+// var isAdmin = confirm("Ты администратор?")
+
+// let age = prompt('Сколько тебе лет?', 30);
+
+// alert(`Тебе ${age} лет!`);
+
+let user = {
+    name: `Aleksandr Lisin`,
+    age: `30 лет`,
+};
+
+const birthYear = 1991;
+
+console.log(user)
+console.log(typeof user)
+console.log(typeof age)
+// console.log(typeof user)
+
+console.log(birthYear)
+console.log(typeof birthYear)
+
+alert(`${user.name} ${user.age}`)
+
+// let x = 343;
+// let y = 23123;
+
+// let result = x + y;
+// console.log(result)
+
+
+let greeting = `Привет,`;
+let userName = `Александр`;
+
+let result = greeting + userName;
+console.log(result);     // Конкатенация
+
+
